e0e166938cdf3285c7e8b2167ce09fad7e603cf9
Sergio Garguir <sergiogarguir@gmail.com>
2023-02-18 15:37:21 -0300

    end section 9

---
+++ dockerfile
@@ -0,0 +1,28 @@
+FROM node:18-alpine3.15 AS deps
+
+RUN apk add --no-cache libc6-compat
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm ci
+# RUN yarn install --frozen-lockfile
+
+# Build the app with cache dependencies
+FROM node:18-alpine3.15 AS builder
+WORKDIR /app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+RUN npm run build
+
+# Production image, compy all the files and run next
+FROM node:18-alpine3.15 AS runner
+
+# Set working directory
+WORKDIR /usr/src/app
+
+COPY package.json package-lock.json ./
+
+RUN npm ci --prod
+
+COPY --from=builder /app/dist ./dist
+
+CMD ["node","dist/main"]
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -6,14 +6,18 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { BadRequestException, InternalServerErrorException, NotFoundException } from '@nestjs/common/exceptions';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
-
+  private defaultLimit: number;
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService,
+  ){
+    this.defaultLimit = configService.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -26,7 +30,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit=10, offset=0 } = paginationDto;
+    const { limit=this.defaultLimit, offset=0 } = paginationDto;
     return this.pokemonModel.find()
       .limit(limit)
       .skip(offset)
